1a29a288c70d007a5330d2b4e3225db663694f5c
Brooke Rose Baer <brooke@chromelia.dev>
2021-12-30 04:11:29 -0500

    Improve risk milk

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,23 +7,27 @@ export default function Home() {
   const handleClick = () => {
     var video = document.getElementById("whatVideo");
     video.play();
+    var audio = document.getElementById("whatAudio");
+    audio.play();
     setTimeout(() => {
       showWhat(true);
-      var audio = document.getElementById("whatAudio");
-      audio.play();
     }, 1000)
+    setTimeout(() => {
+      showWhat(false);
+      video.load();
+    }, 10000)
   }
   return (
     <div onClick={handleClick} id="container" className="container" style={{height: "100%", minHeight: "100vh"}}>
       <h1 className="text-center mt-3">Risk Milk</h1>
       <div className="d-flex justify-content-center">
         <div className="position-relative ">
-          <video id="whatVideo">
+          <video id="whatVideo" height={1000}>
             <source src="milk-drop.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
           {what && (
-            <h1 className="what text-center position-absolute bottom-0 start-50 translate-middle" style={{color: "white", WebkitTextStrokeWidth: "2px", WebkitTextStrokeColor: "black", fontFamily: "Impact" }}>what</h1>
+            <h1 className="what text-center position-absolute bottom-0 start-50 translate-middle" style={{color: "white", WebkitTextStrokeWidth: "5px", WebkitTextStrokeColor: "black", fontFamily: "Impact", fontSize: "200px" }}>what</h1>
           )}
         </div>
         <audio id="whatAudio">
