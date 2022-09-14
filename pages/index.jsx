ae2a7f81c41af59aae855e0ec76aa7a3889923ce
Brooke Rose Baer <me@brookero.se>
2022-09-14 17:26:09 -0400

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,16 +2,23 @@ import React from "react"
 import Image from 'next/image'
 
 export default function Home() {
+  const handleClick = () => {
+    var audio = document.getElementById("riskmilkAudio");
+    audio.play();
+  }
   return (
-    <div id="container" className="container" style={{height: "100%", minHeight: "100vh"}}>
-      <div dangerouslySetInnerHTML={{__html: `<!--SKIM 👉 🤭 -->`}}>
+    <>
+    <div onClick={handleClick} id="container" className="container" style={{height: "100%", minHeight: "100vh", width: "100%", minWidth: "100vw", position: "fixed", overflow: "hidden", zIndex: "-1"}}>
+      <Image src="/riskmilk.png" layout="fill" />
+    </div>
+    <div dangerouslySetInnerHTML={{__html: `<!--SKIM 👉 🤭 -->`}}>
       </div>
       <div className="d-flex justify-content-center">
-        <Image src="/riskmilk.png" layout="fill" />
-        <audio id="riskmilkAudio" autoPlay>
+      <h1 className="text-center mt-5 text-white">Click to</h1>
+        <audio id="riskmilkAudio">
           <source src="shootingstars.mp3" type="audio/mpeg" />
         </audio>
       </div>
-    </div>
+    </>
   )
 }
