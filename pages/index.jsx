1bf6db7ae550e2de2f1b1a9e99bb243fa55f4911
Brooke Rose Baer <me@brookero.se>
2022-09-14 17:08:53 -0400

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,6 @@ export default function Home() {
     <div id="container" className="container" style={{height: "100%", minHeight: "100vh"}}>
       <div dangerouslySetInnerHTML={{__html: `<!--SKIM 👉 🤭 -->`}}>
       </div>
-      <h1 className="text-center mt-3" ></h1>
       <div className="d-flex justify-content-center">
         <Image src="/riskmilk.png" height="90vh" />
         <audio id="riskmilkAudio" autoPlay>
