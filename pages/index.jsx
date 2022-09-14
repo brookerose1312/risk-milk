d34b98ad0547046d5511d9d07493b08a9e7802cc
Brooke Rose Baer <me@brookero.se>
2022-09-14 17:05:14 -0400

    oopsies i wanted to remove the title too

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,6 @@ export default function Home() {
     <div id="container" className="container" style={{height: "100%", minHeight: "100vh"}}>
       <div dangerouslySetInnerHTML={{__html: `<!--SKIM 👉 🤭 -->`}}>
       </div>
-      <h1 className="text-center mt-3" >Risk Milk</h1>
       <div className="d-flex justify-content-center">
         <div className="position-relative ">
           <Image src="riskmilk.png" height="90vh" />
